d72effbc1aba5b539e31350fc333264e57feaab8
vibhorsingal <vibhorsingal154@gmail.com>
2021-05-26 02:12:39 +0530

    authentication complete

--- controllers/resetPasswordController.js
+++ controllers/resetPasswordController.js
@@ -1,4 +1,6 @@
 const Users = require("../models/users")
+const mailgun=require('../config/mailgun')
+const bcrypt=require('bcrypt')
 
 module.exports.getSendOTPController=(req,res)=>{
     res.render('resetPassword')
@@ -8,10 +10,47 @@ module.exports.putOTPController=async (req,res)=>{
     const email=req.body.email
     const user=await Users.findOne({email:email})
     if(user){
-        const otp=Math.floor(Math.random()* 10000)
+        const otp=Math.floor(1000 + Math.random()* 9000)
+        const data={
+            from:'noreply@test.com',
+            to:email,
+            subject:"OTP for resetting password",
+            text:`your OTP is ${otp}`
+        }
+        mailgun.messages().send(data, (error, body) => {
+            console.log(error)
+            console.log(body);
+        }); 
         await Users.findOneAndUpdate({email:email},{otp:otp})
-        const newUser=await Users.findOne({email:email})
-        res.send(newUser)
+        // const newUser=await Users.findOne({email:email})
+        res.render('verifyOtp',{email:email})
+
+        //TODO:(error handling)
+        
+        
+        // res.send(newUser)
     }
 
+}
+
+module.exports.verifyOtpController=async (req,res)=>{
+    const { verifyOtp,email }=req.body
+    const user=await Users.findOne({email:email})
+    const otp=parseInt(verifyOtp)
+    console.log(otp,user)
+
+    if(otp===user.otp){
+        res.render('newPassword',{email:email})
+    }
+}
+
+module.exports.newPasswordController=async (req,res)=>{
+    const {email,newPassword,confirmPassword}=req.body
+    if(newPassword===confirmPassword){
+        const salt=await bcrypt.genSalt(10)
+        const password=await bcrypt.hash(newPassword,salt)
+        console.log(password)
+        await Users.findOneAndUpdate({email:email},{password:password})
+        res.redirect('/')
+    }
 }
--- routes/authenticationRoute.js
+++ routes/authenticationRoute.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const route = express.Router()
 const { getRegisterPage, postRegisterPage } = require('../controllers/register')
-const { getSendOTPController,putOTPController } = require('../controllers/resetPasswordController')
+const { getSendOTPController,putOTPController,verifyOtpController, newPasswordController } = require('../controllers/resetPasswordController')
 
 route.get('/',getRegisterPage)
 route.post('/',postRegisterPage)
 route.get('/user',getSendOTPController)
 route.put('/send-otp',putOTPController)
+route.post('/verify-otp',verifyOtpController)
+route.put('/reset-pswd',newPasswordController)
+
 module.exports=route
